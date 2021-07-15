216d337fc9e4307064dc182884f6fb2fd3946228
nolixul <nadia.kaye@gmail.com>
2021-07-15 16:27:42 +0100

    Add comments list now updates when new comment submitted

--- src/hooks/useComments.js
+++ src/hooks/useComments.js
@@ -8,7 +8,7 @@ const useComments = (review_id) => {
     getComments(review_id).then((commentsFromApi) => {
       setComments(commentsFromApi);
     });
-  }, [review_id]);
+  }, [review_id, comments]);
   return { comments, setComments };
 };
 
